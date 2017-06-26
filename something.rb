6644a3f2b9959ec59636f8ef1cca6c96f649d8d1
michaelhanna5 <peytommanningz@gmail.com>
2017-06-26 13:36:00 -0400

    first, last name final?

--- something.rb
+++ something.rb
@@ -1,12 +1,17 @@
+def firstname()
 
 	print "What is your first name?"
-	fname = gets
-	fname = fname.chomp
+	fname = gets.chomp
+end
+
+def lastname()
 	print "What is your last name?"
-	lname = gets
-	lname = lname.chomp
-
-print "Hello #{fname} #{lname}, Thats a great name."
+	lname = gets.chomp
+end
 
 
+# print "Hello #{fname} #{lname}, Thats a great name."
+first = firstname()
+last = lastname()
 
+print "Hello #{first} #{last}, Thats a great name."
